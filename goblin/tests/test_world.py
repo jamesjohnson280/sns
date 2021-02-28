b000a2f8ac673971cbaf26280ecdbdb8c6a2712e
James Johnson <james.johnson280@gmail.com>
2021-02-27 21:03:50 -0500

    Test world contains correct room

--- goblin/tests/test_world.py
+++ goblin/tests/test_world.py
@@ -10,6 +10,11 @@ class WorldTestCase(unittest.TestCase):
     world.init(Rooms)
 
   def test_init_rooms(self):
-    '''it should contain the 'Dusty Dirt Road' room'''
+    '''it should contain a room'''
     road = world.get('road')
     self.assertIsInstance(road, Room)
+
+  def test_init_rooms(self):
+    '''it should contain the 'Dusty Dirt Road' room'''
+    road = world.get('road')
+    self.assertEqual(road.name, 'Dusty Dirt Road')
