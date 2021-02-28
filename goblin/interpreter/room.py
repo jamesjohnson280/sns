978cc9e47096f5193a74c2f31d1488c65364cb79
James Johnson <james.johnson280@gmail.com>
2021-02-27 20:39:19 -0500

    Add description and exits to the room

--- goblin/interpreter/room.py
+++ goblin/interpreter/room.py
@@ -7,3 +7,20 @@ class Room:
     self.key = key
     self.name = settings['name']
     self.description = settings['description']
+    self.player_visited = False
+    self.exits = {}
+
+  def player_description(self):
+    '''Returns a str containing the description of this Room and its contents
+    from the player's point of view. It returns a verbose description on the 
+    first call but omits `self.description` thereafter. Setting `full` to `True` 
+    overrides this behaviour'''
+    message = '{}\n'.format(self.name)
+    if not self.player_visited:
+      self.player_visited = True
+      message += '{}\n'.format(self.description)
+    return message
+
+  def connect(self, exit, room):
+    '''Connects this Room to another via an exit'''
+    self.exits[exit] = room
--- goblin/tests/test_room.py
+++ goblin/tests/test_room.py
@@ -22,5 +22,30 @@ class RoomTestCase(unittest.TestCase):
     '''it should have a description'''
     self.assertEqual(self.room.description, 'This is a room')
 
+  def test_invalid_init(self):
+    '''it should throw an error on incomplete settings'''
+    with self.assertRaises(KeyError):
+      invalidRoom = Room('invalid', {})
+
+  def test_player_description(self):
+    '''`Room.player_description()` should return the full description'''
+    description = self.room.player_description()
+    self.assertEqual(description, 'Room\nThis is a room\n')
+
+  def test_short_description(self):
+    '''calling `Room.player_description()` a second time should return the short description'''
+    self.room.player_description()
+    description = self.room.player_description()
+    self.assertEqual(description, 'Room\n')
+
+  def test_room_connection(self):
+    '''`Room.connect_to()` should create an exit'''
+    forest = Room('forest', {
+      'name': 'Forest',
+      'description': 'This is a forest'
+    })
+    self.room.connect('east', forest)
+    self.assertEquals(self.room.exits['east'], forest)
+
 if __name__ == '__main__':
   unittest.main()
