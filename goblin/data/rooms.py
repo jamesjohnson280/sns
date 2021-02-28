f2b068d9885549c542e72d5aab047eadfb9552b7
James Johnson <james.johnson280@gmail.com>
2021-02-27 20:45:47 -0500

    Add first room data

---
+++ goblin/data/rooms.py
@@ -0,0 +1,14 @@
+rooms = {
+  'road': {
+    'key': 'road',
+    'name': 'Dusty Dirt Road',
+    'description': 
+    '''
+    You are standing on the dusty road that leads from your village to Blackwood to the east.
+    To the north a path leads in to the forest.
+    ''',
+    'exits': {
+      'north': 'forest'
+    }
+  }
+}
