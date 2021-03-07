2c566010ccbb21ee5eafc4346102784f3ec50219
James Johnson <james.johnson280@gmail.com>
2021-03-06 20:58:31 -0500

    Test parsing edge cases

--- goblin/interpreter/parser.py
+++ goblin/interpreter/parser.py
@@ -1,6 +1,16 @@
 '''Parses player commands in English and converts them to game commands'''
 
+from logging import error
+
+
+class ParserError(Exception):
+  def __init__(self, message):
+    self.message = message
+
 def parse(text):
   '''Attempts to understand a string and returns a game command'''
-  if text == 'quit':
+  s = text.lower()
+  if s == 'quit':
     return {'verb': 'quit'}
+  else:
+    raise ParserError("I don't understand the word '{}.'".format(s))
--- goblin/tests/test_parser.py
+++ goblin/tests/test_parser.py
@@ -8,3 +8,13 @@ class ConsoleTestCase(unittest.TestCase):
     '''it should convert english into game commands'''
     command = parser.parse('quit')
     self.assertEqual(command, {'verb': 'quit'})
+
+  def test_caseless_parse(self):
+    '''it should ignore case on input strings'''
+    command = parser.parse('qUiT')
+    self.assertEqual(command, {'verb': 'quit'})
+
+  def test_parser_error(self):
+    '''It should throw an error if it doesn't understand the input'''
+    with self.assertRaises(parser.ParserError):
+      parser.parse('fr00b')
